a1e803404a68e5589f2d758151f6ed2a3187f437
William Torre <pro.williamtoro@gmail.com>
2024-09-28 13:11:40 -0500

    The socket service is imported to listen and update the data of the list of all by the UPDATE_TODO method when the changes are updated so that it is painted by all the views, a handleMessage function is created to receive the updated list of data and serve it to every component that listens to it

--- src/app/services/globalState/global-state.service.ts
+++ src/app/services/globalState/global-state.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { ConectionWebSocketService } from '../websocket/conection-web-socket.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,28 @@ export class GlobalStateService {
   private todosSubject = new BehaviorSubject<any[]>([]);
   todos$ = this.todosSubject.asObservable();
 
+  constructor(private conectionWebSocketService: ConectionWebSocketService) {
+    // Subscribe to WebSocket messages
+    this.conectionWebSocketService.messages$.subscribe(message => {
+      this.handleMessage(message);
+    });
+  }
+
+  connectWebSocket(url: string, token: string) {
+    this.conectionWebSocketService.connect(url, token);
+  }
+
   updateTodos(todos: any[]) {
     this.todosSubject.next(todos);
+
+    // Send the new state through the WebSocket
+    this.conectionWebSocketService.send({ type: 'UPDATE_TODO', data: todos });
+  }
+
+  private handleMessage(message: any) {
+    // Handle messages received from the WebSocket
+    if (message?.type === 'UPDATE_TODO') {
+      this.todosSubject.next(message.data); // Update the task list
+    }
   }
 }
